c341a19d3788993d2b2c7e30c324c04d6d9d82de
Illyena <nel.huff@gmail.com>
2023-04-24 14:46:18 -0700

    test 7

--- src/main/java/illyena/gilding/holidays/HolidaysInit.java
+++ src/main/java/illyena/gilding/holidays/HolidaysInit.java
@@ -10,7 +10,7 @@ import static illyena.gilding.GildingInit.GILDING;
 import static illyena.gilding.compat.Mod.MODS;
 
 public class HolidaysInit implements ModInitializer {
-    public static final String MOD_ID = "holiday";
+    public static final String MOD_ID = "holidays";
     public static final String MOD_NAME = "Holidays";
     public static final String VERSION = "1.18.2 dev 0.8.0";
 
